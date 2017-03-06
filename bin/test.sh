7377104d9ab066f27858964290bb3e6bd9b3d743
Evan Summers <evan.summers@gmail.com>
2017-03-06 23:45:10 +0000

    initial

--- bin/test.sh
+++ bin/test.sh
@@ -5,7 +5,4 @@
   redis-cli hset $key about 'https://en.wikipedia.org/wiki/Richard_Feynman'
   redis-cli hset $key born '1918-05-11'
   redis-cli hset $key died '1988-02-15'
-
-  key=$key npm start 
-
-
+  key=$key npm start
